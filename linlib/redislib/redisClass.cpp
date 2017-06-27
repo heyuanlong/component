5af000f1c98dd7489b1b8f890e36632fbb2dc2cd
heyuanlong <1023417614@qq.com>
2017-06-27 21:20:32 +0800

    sdfsdfs

--- linlib/redislib/redisClass.cpp
+++ linlib/redislib/redisClass.cpp
@@ -2,7 +2,7 @@
 
 
 
-redisClass::redisClass(const char *host,const int port ,const char * auth)
+redisClass::redisClass()
 {
 	m_rc = NULL;
 }
@@ -31,11 +31,66 @@ int redisClass::init(const char *host,const int port ,const char * auth)
 	if (strlen(auth) > 0){
 		char authstr[120];
 		sprintf(authstr,"auth %s",auth);
-		redisReply *reply = redisCommand(m_rc,authstr);
+		redisReply *reply = (redisReply *)redisCommand(m_rc,authstr);
 		if (reply->type == REDIS_REPLY_ERROR){
 			//printf("Error: %s\n", reply->str);
+			freeReplyObject(reply);
 			return -1;
 		}
+		freeReplyObject(reply);
 	}
 	return 0;
+}
+
+redisContext* redisClass::getRedisCT()
+{
+	return m_rc;
+}
+
+int redisClass::ping()
+{
+	int ret = 0;
+	redisReply *reply = (redisReply *)redisCommand(m_rc,"ping");
+	if (reply->type == REDIS_REPLY_ERROR){
+		//printf("Error: %s\n", reply->str);
+		ret = -1;
+	}
+	freeReplyObject(reply);
+	return ret;
+}
+int redisClass::set(const char *key,const char* value)
+{
+	int ret = 0;
+	redisReply *reply = (redisReply *)redisCommand(m_rc,"set %s %s",key,value);
+	if (reply->type == REDIS_REPLY_ERROR){
+		//printf("Error: %s\n", reply->str);
+		ret = -1;
+	}
+	freeReplyObject(reply);
+	return ret;
+}
+int redisClass::get(const char *key,char *value)
+{
+	int ret = 0;
+	redisReply *reply = (redisReply *)redisCommand(m_rc,"get %s",key);
+	if (reply->type == REDIS_REPLY_ERROR){
+		//printf("Error: %s\n", reply->str);
+		ret = -1;
+	}
+	strcpy(value,reply->str);
+	freeReplyObject(reply);
+	return ret;
+}
+
+int redisClass::incr(const char *key,long long *value)
+{
+	int ret = 0;
+	redisReply *reply = (redisReply *)redisCommand(m_rc,"incr %s",key);
+	if (reply->type == REDIS_REPLY_ERROR){
+		//printf("Error: %s\n", reply->str);
+		ret = -1;
+	}
+	*value = reply->integer;
+	freeReplyObject(reply);
+	return ret;
 }
